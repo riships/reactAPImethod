cadb1e770c8db6b04c85950cf8866564594115f1
Mohd Wazid <mohd.wazid.db@gmail.com>
2023-04-15 11:23:36 +0530

    Updates

---
+++ precache-manifest.752da7de89ad641df90e2b634eb1bf21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b407e854fc757efe837",
+    "url": "/reactAPImethod/static/css/main.a0982765.chunk.css"
+  },
+  {
+    "revision": "2b407e854fc757efe837",
+    "url": "/reactAPImethod/static/js/main.2b407e85.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/reactAPImethod/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/reactAPImethod/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "edda6e4246f871b4409b",
+    "url": "/reactAPImethod/static/js/runtime~main.edda6e42.js"
+  },
+  {
+    "revision": "1a8cdc4c6756b036e78a4333f1f2cd69",
+    "url": "/reactAPImethod/index.html"
+  }
+];
